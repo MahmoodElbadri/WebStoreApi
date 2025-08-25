6f00484e886a16a7a3133a3820e5de010004928a
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-08-25 23:16:46 +0300

    Add configuration settings and improve Program.cs structure
    Updated Program.cs to read Language, SecretKey, and Logging level from appsettings.json. Enhanced code clarity with console output for configuration values. Refined service registration and middleware setup for better organization. Modified appsettings.json to include new entries for Language, SecretKey, and Country.

--- WebStoreApi/Program.cs
+++ WebStoreApi/Program.cs
@@ -1,38 +1,37 @@
 
-namespace WebStoreApi
+var builder = WebApplication.CreateBuilder(args);
+
+//reading variables from appsettings.json
+
+var lang = builder.Configuration["Language"];
+var key = builder.Configuration["SecretKey"];
+var log = builder.Configuration["Logging:LogLevel:Default"];
+
+Console.WriteLine($"lang is {lang}, key is {key}, log is {log}");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
